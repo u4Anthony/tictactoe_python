92a9d0086157238c86489c1412e82e52e9dcd5da
Anthony <ahaddadkc@gmail.com>
2023-06-03 01:36:51 -0400

    Add GUI Functionality
    Add functionality for GUI to change button text and color when clicked.

--- tictactoe.py
+++ tictactoe.py
@@ -8,6 +8,9 @@ root.title("Tic Tac Toe")
 # Add Label
 tk.Label(root, text="Tic Tac Toe", font=('Ariel', 25)).pack()
 
+# Set the first character
+current_chr = "X"
+
 # Add play area
 play_area = tk.Frame(root, width=300, height=300, bg='white')
 XO_points = []
@@ -16,8 +19,18 @@ class XOPoint:
         self.x = x
         self.y = y
         self.value = None
-        self.button = tk.Button(play_area, text="", width=10, height=5)
+        self.button = tk.Button(play_area, text="", width=10, height=5, command=self.set)
         self.button.grid(row=x, column=y)
+
+    def set(self):
+        global current_chr
+        if not self.value:
+            self.button.configure(text=current_chr, bg='snow', fg='black')
+            self.value = current_chr
+            if current_chr == "X":
+                current_chr = "O"
+            elif current_chr == "O":
+                current_chr = "X"
 
     def reset(self):
         self.button.configure(text="", bg='white')
