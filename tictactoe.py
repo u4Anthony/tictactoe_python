01a70090938025787694946215d6c12c201f2584
Anthony <ahaddadkc@gmail.com>
2023-06-03 01:32:00 -0400

    Create Label and Play Area
    Within the GUI create a play area that the tic tac toe boxes will sit.
    Additionally create a label for the game.

--- tictactoe.py
+++ tictactoe.py
@@ -5,4 +5,28 @@ root = tk.Tk()
 root.resizable(False, False)
 root.title("Tic Tac Toe")
 
+# Add Label
+tk.Label(root, text="Tic Tac Toe", font=('Ariel', 25)).pack()
+
+# Add play area
+play_area = tk.Frame(root, width=300, height=300, bg='white')
+XO_points = []
+class XOPoint:
+    def __init__(self, x, y): 
+        self.x = x
+        self.y = y
+        self.value = None
+        self.button = tk.Button(play_area, text="", width=10, height=5)
+        self.button.grid(row=x, column=y)
+
+    def reset(self):
+        self.button.configure(text="", bg='white')
+        self.value = None
+
+for x in range(1, 4):
+    for y in range(1, 4):
+        XOPoint(x, y)
+
+play_area.pack(pady=10, padx=10)
+
 root.mainloop()
